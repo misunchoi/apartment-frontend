bf9244661207813c696a9032605b7188dca64490
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-26 08:52:14 -0800

    navbar header

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
       if (this.props.auth.loggedIn()) {
         return(
           <nav style={{marginBottom: '20px'}} className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">Apartment App</a>
+            <a className="navbar-brand" href="/">Apartment Hunters</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation" onClick={this.handleToggle}>
               <span className="navbar-toggler-icon"></span>
             </button>

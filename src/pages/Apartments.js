2cf962b70d6283d22cda1ccf47ca536b6293f5de
Mi Sun Choi <misunchoi@Mis-MacBook-Air.attlocal.net>
2020-04-24 16:08:05 -0700

    fix: Add loader

--- src/pages/Apartments.js
+++ src/pages/Apartments.js
@@ -1,60 +1,71 @@
-import React, { Component } from 'react';
-import { getApartments } from '../api';
+import React, { Component } from "react";
+import { getApartments } from "../api";
 
 const cards = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center'
-}
-
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "center"
+};
 
 class Apartments extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        apartments:[]
-    }
+      apartments: []
+    };
   }
 
   componentWillMount() {
-    getApartments()
-    .then(APIapartments => {
+    getApartments().then(APIapartments => {
       this.setState({
-          apartments: APIapartments
-      })
-    })
+        apartments: APIapartments
+      });
+    });
   }
 
   render() {
     console.log(this.state.apartments);
+
     return (
       <div>
-        <h3>All Apartments</h3><br/>
+        <h3>All Apartments</h3>
+        <br />
         <div style={cards}>
-          {this.state.apartments.length === 0 
-            ? 
-              <div>Loading Apartments...</div> 
-
-            : 
-              this.state.apartments.map(el => {
-                return (
-                  <div key={el.id} className="card border-primary mb-3" style={{minWidth: "20rem", margin: '20px'}}>
-                    <div className="card-header">Apartment No.{el.id}</div>
-                    <div className="card-body">
-                      <h4 className="card-title">
-                        {el.city}, {el.state}
-                      </h4>
-                      <p className="card-text">
-                        Manager: {el.building_manager} <br/>
-                        Phone: {el.manager_phone} <br/><br/>
-                        <a className="btn btn-primary" href={`/apartments/${el.id}`}>Details</a>
-                      </p>
-                    </div>
+          {this.state.apartments.length === 0 ? (
+            <div>
+              <div class="loader"></div>
+              Loading...
+            </div>
+          ) : (
+            this.state.apartments.map(el => {
+              return (
+                <div
+                  key={el.id}
+                  className="card border-primary mb-3"
+                  style={{ minWidth: "20rem", margin: "20px" }}
+                >
+                  <div className="card-header">Apartment No.{el.id}</div>
+                  <div className="card-body">
+                    <h4 className="card-title">
+                      {el.city}, {el.state}
+                    </h4>
+                    <p className="card-text">
+                      Manager: {el.building_manager} <br />
+                      Phone: {el.manager_phone} <br />
+                      <br />
+                      <a
+                        className="btn btn-primary"
+                        href={`/apartments/${el.id}`}
+                      >
+                        Details
+                      </a>
+                    </p>
                   </div>
-                )
-              })
-          }
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     );

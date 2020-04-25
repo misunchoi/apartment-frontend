73b5acf173e88a46067aaeee36e382fa17748a8d
Mi Sun Choi <misunchoi@Mis-MacBook-Air.attlocal.net>
2020-04-24 17:06:39 -0700

    refactor login and register form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import AuthService from "../services";
+import { Link } from "react-router-dom";
 
-import AuthService from '../services'
-
-const form = {
-  marginTop: "30px"
-}
-
-const group = {
-  display: "flex",
-  flexDirection: 'column',
-  margin: 'auto'
-}
-
-const input = {
-  width: "300px",
-}
-
-const label = {
-  textAlign: 'left',
-  marginTop: "10px"
-}
-
+export const formStyles = {
+  formWrapper: {
+    marginTop: "30px",
+    backgroundColor: "rgba(255, 255, 255,0.65)",
+    padding: "1em",
+    width: "350px",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: 'center'
+  },
+  group: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto"
+  },
+  input: {
+    width: "300px"
+  },
+  label: {
+    textAlign: "left",
+    marginTop: "10px"
+  }
+};
 
 class Login extends Component {
-  constructor(props){
-    super(props)
-    this.auth = new AuthService()
+  constructor(props) {
+    super(props);
+    this.auth = new AuthService();
     this.state = {
       loginSuccess: false,
       passwordValidation: "form-control",
@@ -38,101 +44,138 @@ class Login extends Component {
           email: "",
           password: ""
         }
-      }
-    }
+      },
+      submitted: false,
+      invalidInput: false,
+    };
   }
 
-  onChange = (e) => {
-    let { form } = this.state
-    form.user[e.target.name] = e.target.value
-    this.setState ({
+  onChange = e => {
+    let { form } = this.state;
+    form.user[e.target.name] = e.target.value;
+    this.setState({
       form
-    })
-    this.handleValidation(e)
-  }
+    });
+    this.state.submitted && this.handleValidation(e);
+  };
 
-  onSubmit = (e) => {
-    e.preventDefault()
+  onSubmit = e => {
+    e.preventDefault();
+    this.handleValidation(e);
+
     this.setState({
       errorMessage: "Please wait...",
-      messageColor: "blue"
-    })
-    this.auth.login(this.state.form)
-    .then(json => {
-      console.log("got to second then:", json)
-      if(json.error) {
-        this.setState({
-          errors: json.error,
-          errorMessage: "Please enter a valid email and/or password.",
-          messageColor: "red"
-        })
-        console.log("!! ERRORS !!", json.errors);
-      } else {
-        this.props.refresh()
-      }
-    })
-  }
+      messageColor: "blue",
+      submitted: true,
+    });
+      this.auth.login(this.state.form).then(json => {
+        if (json.error) {
+          this.setState({
+            errors: json.error,
+            errorMessage: "You have entered an invalid username or password.",
+            messageColor: "red"
+          });
+        } else {
+          this.props.refresh();
+        }
+      });
+  };
 
-  handleValidation = (e) => {
-    e.preventDefault()
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(this.state.form.user.email) && this.state.form.user.password.length < 6) {
+  handleValidation = e => {
+    e.preventDefault();
+    if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+        this.state.form.user.email
+      ) &&
+      this.state.form.user.password.length < 6
+    ) {
       this.setState({
+        invalidInput: true,
         emailValidation: "form-control is-invalid",
         passwordValidation: "form-control is-invalid"
-      })
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(this.state.form.user.email)) {
+      });
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+        this.state.form.user.email
+      )
+    ) {
       this.setState({
+        invalidInput: true,
         emailValidation: "form-control is-invalid",
-        passwordValidation: "form-control",
-      })
+        passwordValidation: "form-control"
+      });
     } else if (this.state.form.user.password.length < 6) {
       this.setState({
+        invalidInput: true,
         emailValidation: "form-control",
-        passwordValidation: "form-control is-invalid",
-      })
+        passwordValidation: "form-control is-invalid"
+      });
     } else {
       this.setState({
+        invalidInput: false,
         emailValidation: "form-control",
-        passwordValidation: "form-control",
-      })
+        passwordValidation: "form-control"
+      });
     }
-  }
+  };
 
-  render () {
-    let {
-      email,
-      password
-    } = this.state.form.user
+  render() {
+    let { email, password } = this.state.form.user;
+    const { formWrapper, form, group, label, input } = formStyles;
 
-
-    return(
-      <div style={form}>
-        
+    return (
+      <div style={formWrapper}>
         <h4>Login</h4>
-        <form onSubmit={this.onSubmit}>
-
+        {this.state.errorMessage && (
+            <p style={{ color: this.state.messageColor }}>
+              {this.state.errorMessage}
+            </p>
+          )}
+        <form style={form} onSubmit={this.onSubmit}>
           <div style={group} class="form-group">
             <label style={label}>Email</label>
-            <input style={input} className={this.state.emailValidation} onChange={this.onChange} name="email" value={email} type="email"/>
-            <div class="invalid-feedback">Please enter a valid email address</div>
+            <input
+              style={input}
+              className={this.state.emailValidation}
+              onChange={this.onChange}
+              name="email"
+              value={email}
+            />
+            <div class="invalid-feedback">
+              Please enter a valid email address
+            </div>
           </div>
 
           <div style={group} class="form-group">
             <label style={label}>Password</label>
-            <input style={input} className={this.state.passwordValidation} onChange={this.onChange} name="password" value={password} type="password"/>
-            <div class="invalid-feedback">Password needs to be at least 6 characters long</div>
+            <input
+              style={input}
+              className={this.state.passwordValidation}
+              onChange={this.onChange}
+              name="password"
+              value={password}
+              type="password"
+            />
+            <div class="invalid-feedback">
+              Password needs to be at least 6 characters long
+            </div>
           </div>
 
-          <button style={{margin: '20px'}} type="submit" className="btn btn-primary">Login</button>
-          <p style={{color: this.state.messageColor}}>{this.state.errorMessage}</p>
-          <p>Don't have an account? Register <a href="/users/new">here</a></p>
+          <button
+            style={{ margin: "20px" }}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Login
+          </button>
+
+          <p>
+            Don't have an account? Register <Link to="/users/new">here</Link>
+          </p>
         </form>
       </div>
-    )
+    );
   }
-
-  
-
 }
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,29 +1,12 @@
-import React, { Component } from 'react';
-import AuthService from '../services'
-
-const form = {
-  marginTop: "30px"
-}
-
-const group = {
-  display: "flex",
-  flexDirection: 'column',
-  margin: 'auto'
-}
-
-const input = {
-  width: "300px",
-  marginBottom: "10px"
-}
-
-const label = {
-  textAlign: 'left'
-}
+import React, { Component } from "react";
+import AuthService from "../services";
+import { formStyles } from "./Login";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
-  constructor(props){
-    super(props)
-    this.auth = new AuthService()
+  constructor(props) {
+    super(props);
+    this.auth = new AuthService();
     this.state = {
       registerSuccess: false,
       passwordValidation: "form-control",
@@ -37,114 +20,150 @@ class Register extends Component {
           password: ""
         }
       }
-    }
+    };
   }
 
-  render () {
-    let {
-      email,
-      password
-    } = this.state.form.user
-    return(
-
-      <div style={form}>
-        <h4>New Account</h4>
-        <form onSubmit={this.onSubmit}>
-
-          <div style={group} class="form-group">
-            <label style={label}>Email</label>
-            <input style={input} className={this.state.emailValidation} onChange={this.onChange} name="email" value={email} type="email"/>
-            <div class="invalid-feedback">Please enter a valid email address</div>
-          </div>
-
-          <div style={group} class="form-group">
-            <label style={label}>Password</label>
-            <input style={input} className={this.state.passwordValidation} onChange={this.onChange} name="password" value={password} type="password"/>
-            <div class="invalid-feedback">Password needs to be at least 6 characters long</div>
-          </div>
-
-          <p style={{color: this.state.messageColor}}>{this.state.errorMessage}</p>
-
-          <button style={{margin: '20px'}} type="submit" className="btn btn-primary">Register</button>
-          <p>Already registered? Login <a href="/login">here</a></p>
-        </form>
-      </div>
-    )
-  }
-
-  onChange = (e) => {
-    let { form } = this.state
-    form.user[e.target.name] = e.target.value
-    this.setState ({
+  onChange = e => {
+    let { form } = this.state;
+    form.user[e.target.name] = e.target.value;
+    this.setState({
       form
-    })
-    this.handleValidation(e)
-  }
+    });
+    this.handleValidation(e);
+  };
 
-  onSubmit = (e) => {
-    e.preventDefault()
-    if (this.state.form.user.email === "" || this.state.form.user.password === "") {
+  onSubmit = e => {
+    e.preventDefault();
+    if (
+      this.state.form.user.email === "" ||
+      this.state.form.user.password === ""
+    ) {
       this.setState({
         errorMessage: "Email and/or password cannot be blank.",
         messageColor: "red"
-      })
+      });
     } else {
       this.setState({
         errorMessage: "Please wait...",
         messageColor: "blue"
-      })
+      });
 
-      this.auth.register(this.state.form)
-      .then(json => {
-        console.log("got to second then:", json.errors)
-        if (this.state.emailValidation === "form-control is-invalid" || this.state.passwordValidation === "form-control is-invalid") {
+      this.auth.register(this.state.form).then(json => {
+        if (
+          this.state.emailValidation === "form-control is-invalid" ||
+          this.state.passwordValidation === "form-control is-invalid"
+        ) {
           this.setState({
             errors: json.errors,
             errorMessage: "Please enter a valid email or password.",
             messageColor: "red"
-          })
-          console.log("!! ERRORS !!", json.errors);
+          });
         } else if (json.errors === undefined) {
-          this.props.refresh()
+          this.props.refresh();
         } else if (json.errors.email) {
           this.setState({
             errors: json.errors,
             errorMessage: "The email is already registered.",
             messageColor: "red"
-          })
+          });
         } else {
-          this.props.refresh()
+          this.props.refresh();
         }
-      })
+      });
     }
+  };
 
-  }
-
-  handleValidation = (e) => {
-    e.preventDefault()
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(this.state.form.user.email) && this.state.form.user.password.length < 6) {
+  handleValidation = e => {
+    e.preventDefault();
+    if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+        this.state.form.user.email
+      ) &&
+      this.state.form.user.password.length < 6
+    ) {
       this.setState({
         emailValidation: "form-control is-invalid",
         passwordValidation: "form-control is-invalid"
-      })
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(this.state.form.user.email)) {
+      });
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+        this.state.form.user.email
+      )
+    ) {
       this.setState({
         emailValidation: "form-control is-invalid",
-        passwordValidation: "form-control",
-      })
+        passwordValidation: "form-control"
+      });
     } else if (this.state.form.user.password.length < 6) {
       this.setState({
         emailValidation: "form-control",
-        passwordValidation: "form-control is-invalid",
-      })
+        passwordValidation: "form-control is-invalid"
+      });
     } else {
       this.setState({
         emailValidation: "form-control",
-        passwordValidation: "form-control",
-      })
+        passwordValidation: "form-control"
+      });
     }
-  }
+  };
 
+  render() {
+    let { email, password } = this.state.form.user;
+    const { formWrapper, form, group, label, input } = formStyles;
+
+    return (
+      <div style={formWrapper}>
+        <h4>Create Account</h4>
+        <form style={form} onSubmit={this.onSubmit}>
+          <div style={group} class="form-group">
+            <label style={label}>Email</label>
+            <input
+              style={input}
+              className={this.state.emailValidation}
+              onChange={this.onChange}
+              name="email"
+              value={email}
+              type="email"
+            />
+            <div class="invalid-feedback">
+              Please enter a valid email address
+            </div>
+          </div>
+
+          <div style={group} class="form-group">
+            <label style={label}>Password</label>
+            <input
+              style={input}
+              className={this.state.passwordValidation}
+              onChange={this.onChange}
+              name="password"
+              value={password}
+              type="password"
+            />
+            <div class="invalid-feedback">
+              Password needs to be at least 6 characters long
+            </div>
+          </div>
+          {this.state.errorMessage && (
+            <p style={{ color: this.state.messageColor }}>
+              {this.state.errorMessage}
+            </p>
+          )}
+
+          <button
+            style={{ margin: "20px" }}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Register
+          </button>
+          <p>
+            Already registered? Login <Link to="/login">here</Link>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Register;
